926730bc0e12d2587ec68fd2da54c3ee4a73dcd9
Gerson Aranibar <aranibargerson28@gmail.com>
2021-09-16 13:42:51 -0500

    se agrego el layout

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import "./footer.scss";
+export function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>Todos los derechos reservados | 2021</p>
+      </div>
+    </footer>
+  );
+}
